22564e616ee304f6f9b2de6872ee720b2632e1b0
Muhammad Emara <muhammad.emara@gmail.com>
2019-12-19 12:23:23 +0400

    After Adding Model and Contoller for Posts

--- app/User.php
+++ app/User.php
@@ -36,4 +36,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+
+    public function checkRoles($roles)
+    {
+        if (!is_array($roles)) {
+            $roles = [$roles];
+        }
+
+        if (!$this->hasAnyRole($roles)) {
+            auth()->logout();
+            abort(404);
+        }
+    }
+
+    public function hasAnyRole($roles): bool
+    {
+        return (bool) $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasRole($role): bool
+    {
+        return (bool) $this->roles()->where('name', $role)->first();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PostController@all');
+Route::get('/posts/{post}', 'PostController@single');
