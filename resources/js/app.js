12223ce951813f25585d60bf00ad236009996488
Muhammad Emara <muhammad.emara@gmail.com>
2019-12-22 08:38:33 +0400

    Update APPJS Route

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,9 @@ require('./bootstrap');
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Homepage from './components/Homepage'
+import Create from './components/Create'
 import Read from './components/Read'
+import Update from './components/Update'
 
 //window.Vue = require('vue');
 
@@ -36,19 +38,29 @@ import Read from './components/Read'
 });*/
 
 
-
-
-
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
     routes: [{
-        path: '/admin/dashboard',
-        name: 'read',
-        component: Read,
-        props: true
-    }, ],
+            path: '/admin/dashboard',
+            name: 'read',
+            component: Read,
+            props: true
+        },
+        {
+            path: '/admin/create',
+            name: 'create',
+            component: Create,
+            props: true
+        },
+        {
+            path: '/admin/update',
+            name: 'update',
+            component: Update,
+            props: true
+        },
+    ],
 });
 
 const app = new Vue({
