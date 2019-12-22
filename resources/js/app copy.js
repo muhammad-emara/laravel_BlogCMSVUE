3a42fd18fd436c1980582800bbbf1e42eb4510a6
Muhammad Emara <muhammad.emara@gmail.com>
2019-12-22 08:29:51 +0400

    Init AdminView And HomeVue

--- resources/js/app copy.js
+++ resources/js/app copy.js
@@ -5,12 +5,8 @@
  */
 
 require('./bootstrap');
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Homepage from './components/Homepage'
-import Read from './components/Read'
 
-//window.Vue = require('vue');
+window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -23,7 +19,7 @@ import Read from './components/Read'
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -31,30 +27,6 @@ import Read from './components/Read'
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-/*const app = new Vue({
-    el: '#app',
-});*/
-
-
-
-
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [{
-        path: '/admin/dashboard',
-        name: 'read',
-        component: Read,
-        props: true
-    }, ],
-});
-
 const app = new Vue({
     el: '#app',
-    router,
-    components: {
-        Homepage
-    },
 });
